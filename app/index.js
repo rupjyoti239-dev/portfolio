90591071ad38b16396790cce9cd0d438f4bcc25d
Rupjyoti Sarma <rup.sarma23@gmail.com>
2023-10-04 22:59:54 +0530

    refactor - index.js

--- app/index.js
+++ app/index.js
@@ -12,3 +12,45 @@ if(clsBtn){
     nav.classList.remove('active');
   })
 }
+
+
+
+// filter
+(
+  ()=>{
+const buttons = document.querySelectorAll('.btn-tab');
+const storeData = document.querySelectorAll('.card');
+buttons.forEach((button)=>{
+
+
+    button.addEventListener('click', (e)=>{
+      e.preventDefault();
+      const filter = e.target.dataset.filter;
+
+
+      
+    storeData.forEach((item)=>{
+      if(filter === "all"){
+        item.style.display = "block"
+      }
+      else{
+        if(item.classList.contains(filter)){
+          item.style.display = "block"
+        }
+        else{
+          item.style.display = "none"
+        }
+      }
+
+
+    })
+
+
+
+
+
+    })
+   })
+
+  }
+)()
